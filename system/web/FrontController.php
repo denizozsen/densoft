5e6d760b6cf1a917df6c69b4df925418b844e960
Deniz <deniz.ozsen@gmail.com>
2011-05-02 00:21:56 +0400

    Skeleton of class system_web_FrontController

---
+++ system/web/FrontController.php
@@ -0,0 +1,44 @@
+<?php
+
+abstract class system_web_FrontController
+{
+	public function setRequest(system_web_Request $request)
+	{
+
+	}
+
+	public function addControllers()
+	{
+
+	}
+
+	public function preHandleActions()
+	{
+
+	}
+
+	public function postHandleActions()
+	{
+
+	}
+
+	public function preRender()
+	{
+
+	}
+
+	public function postRender($html)
+	{
+
+	}
+
+	public /*final*/ addController($pageRegion, system_mvc_controller $controller)
+	{
+		// TODO - implement addController
+	}
+
+	public /*final*/ getControllers($pageRegion)
+	{
+		// TODO - implement getControllers
+	}
+}
