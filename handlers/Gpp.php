b485ba044f1b25ef2e7f9de6fede72025b66a904
Deniz Ozsen <deniz.ozsen@gmail.com>
2011-08-05 10:28:54 +0400

    Add experimental "Good Programming Practices" page

---
+++ handlers/Gpp.php
@@ -0,0 +1,48 @@
+<?php
+
+class handlers_Gpp extends system_web_PageRequestHandler
+{
+	public function configurePage(system_web_Page $page)
+	{
+		$page->setTemplate('templates/default.tpl');
+		$page->setTitle('Good Programming Practices - ' . Configuration::getInstance()->siteName());
+		$page->setMainHeading('Good Programming Practices');
+		if (!is_null(Configuration::getInstance()->companyLogoPath())) {
+			$rootUrl = Configuration::getInstance()->getRootUrl();
+			$page->setSiteLogo(sprintf('<a href="%s"><img src="%s" /></a>',
+				$rootUrl, $rootUrl . Configuration::getInstance()->companyLogoPath()));
+		}
+		if (!is_null(Configuration::getInstance()->footerHtml())) {
+			$page->addController(system_web_PageRegion::FOOTER,
+				new system_mvc_StaticController(Configuration::getInstance()->footerHtml()));
+		}
+		
+		$navController =
+			new modules_navigation_NavigationLinksController(null,
+				system_web_Services::getInstance()->getRequest());
+		$page->addController(system_web_PageRegion::MAIN_NAV, $navController);
+	
+		///////  THIS CODE SHOULD BE IN SOME CONTROLLER  ////////////
+		
+		// Generate content
+		$content = <<<EOF
+			<h2>Overview of Good Programming Practices</h2>
+			<p>
+				[A short introduction]
+			</p>
+			
+			<h2>Topics</h2>
+			<ul>
+				<li>Code Formatting</li>
+				<li>In-Code documentation</li>
+				<li>Testing Practices</li>
+				<li>Peer Review</li>
+			</ul>
+EOF;
+		
+		/////////////////////////////////////////////////////////////////////
+	
+		$page->addController(system_web_PageRegion::CONTENT,
+			new system_mvc_StaticController($content));
+	}
+}
