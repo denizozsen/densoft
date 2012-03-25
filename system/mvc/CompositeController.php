de531197cf5b3963c97a1979db0856d1a3cc0450
Deniz <deniz.ozsen@gmail.com>
2012-03-25 08:18:06 +0400

    Add new method renderWithPrefixAndSuffix to CompositeController

--- system/mvc/CompositeController.php
+++ system/mvc/CompositeController.php
@@ -1,8 +1,10 @@
 <?php
 
 /**
- * TODO - Document this class
- *
+ * A composite controller is a controller that has any number of children, and
+ * whose controller method calls are delegated to each of its children, in the
+ * order in which those children were added via addChildController().
+ * 
  * @author Deniz …zsen
  */
 class system_mvc_CompositeController extends system_mvc_Controller
@@ -12,7 +14,7 @@ class system_mvc_CompositeController extends system_mvc_Controller
 	/**
 	 * Creates a new composite controller, with constructor arguments used
 	 * as children.
-	 *
+	 * 
 	 * @throws InvalidArgumentException if any argument is not of type system_web_Controller
 	 */
 	public function __construct()
@@ -33,11 +35,19 @@ class system_mvc_CompositeController extends system_mvc_Controller
 		}
 	}
 	
-	public function addChildController($childController)
+	/**
+	 * Adds a controller to the composite's list of child controllers.
+	 * 
+	 * @param system_mvc_Controller $childController the child controller to add
+	 */
+	public function addChildController(system_mvc_Controller $childController)
 	{
 		$this->childControllers[] = $childController;
 	}
 	
+    /* (non-PHPdoc)
+     * @see system_mvc_Controller::handleActions()
+     */
     public function handleActions()
     {
         foreach($this->childControllers as $child) {
@@ -45,22 +55,37 @@ class system_mvc_CompositeController extends system_mvc_Controller
         }
     }
 	
+    /* (non-PHPdoc)
+     * @see system_mvc_Controller::render()
+     */
     public function render()
     {
-        switch(count($this->childControllers))
-        {
-        	case 0: // if no children, do not render anything
-        		break;
-        	case 1: // if one child, render it, without enclosing it with any other tag
-            	$this->childControllers[0]->render();
-        		break;
-        	default: // if multiple children, enclose each child with a div
-            	foreach($this->childControllers as $child) {
-            		echo '<div>' . PHP_EOL;
-            		$child->render();
-            		echo '</div>' . PHP_EOL;
-            	}
-        		break;
+        if (count($this->childControllers) == 1) {
+            // We will render single-child composites without enclosing the
+            // single controller with any other tag
+            $this->renderWithPrefixAndSuffix('', '');
+        } else {
+        	// If there are several children, enclose each child with a div
+        	// Note: this also caters for the case where the composite does not
+        	//       have any children: in that case, nothing is rendered
+            $this->renderWithPrefixAndSuffix('<div>' . PHP_EOL, PHP_EOL . '</div>');
         }
+    }
+    
+    /**
+     * Extends the normal rendering process (which renders each child, in turn)
+     * by printing a specified prefix before each child's rendering output and
+     * a specified suffix after each child's rendering output.
+     * 
+     * @param string $childPrefix a string to be printed before each child's rendering output
+     * @param string $childSuffix a string to be printed after each child's rendering output
+     */
+    public function renderWithPrefixAndSuffix($childPrefix, $childSuffix)
+    {
+    	foreach($this->childControllers as $child) {
+    		echo $childPrefix;
+    		$child->render();
+    		echo $childSuffix;
+    	}
     }
 }
